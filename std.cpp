49cab7f9e4f054751a832894e235c150e839bf21
Wing Gu <wmyw96@126.com>
2014-06-27 18:49:03 +0800

    std
    std for srm

---
+++ std.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+#define X first
+#define Y second
+#define mk make_pair
+#define lx (x << 1)
+#define rx (lx + 1)
+#define rep(i, s, t) for (int i = (s); i <= (t); ++i)
+#define per(i, s, t) for (int i = (s); i >= (t); --i)
+#define REP(i, n) rep(i, 1, n)
+#define PER(i, n) per(i, n, 1)
+typedef long long LL;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef map<int, int> mi;
+typedef mi::iterator mii;
+
+int main(){
+
+}
